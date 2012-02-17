fe952e49d5c5e05441f8936f2d42c1c1508fdc8d
yohang <yohan@giarelli.org>
2012-02-17 17:29:32 +0100

    Updated EmailContactListener

--- EventDispatcher/Listener/EmailContactListener.php
+++ EventDispatcher/Listener/EmailContactListener.php
@@ -2,6 +2,8 @@
 
 namespace FrequenceWeb\Bundle\ContactBundle\EventDispatcher\Listener;
 
+use Symfony\Bundle\FrameworkBundle\Translation\Translator;
+
 use FrequenceWeb\Bundle\ContactBundle\EventDispatcher\Event\MessageSubmitEvent;
 
 class EmailContactListener
@@ -12,18 +14,35 @@ class EmailContactListener
     protected $mailer;
 
     /**
+     * @var \Symfony\Bundle\FrameworkBundle\Translation\Translator
+     */
+    protected $translator;
+
+    /**
      * @var array
      */
     protected $config;
 
-    public function __construct(\Swift_Mailer $mailer, array $config)
+    public function __construct(\Swift_Mailer $mailer, Translator $translator, array $config)
     {
-        $this->mailer = $mailer;
-        $this->config = $config;
+        $this->mailer     = $mailer;
+        $this->translator = $translator;
+        $this->config     = $config;
     }
 
     public function onMessageSubmit(MessageSubmitEvent $event)
     {
+        $contact = $event->getContact();
 
+        $message = new \Swift_Message(
+            $this->translator->trans($this->config['subject']),
+            $contact->body
+        );
+
+        $message->addFrom($this->config['from']);
+        $message->addReplyTo($contact->email, $contact->name);
+        $message->addTo($this->config['to']);
+
+        $this->mailer->send($message);
     }
 }
