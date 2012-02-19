2a21e9b96f2dcb1d2b09c7836ecabef4f516d0e8
Yohan G <yohan@giarelli.org>
2012-02-19 18:56:56 +0100

    Added authors

--- Form/ContactType.php
+++ Form/ContactType.php
@@ -5,8 +5,17 @@ namespace FrequenceWeb\Bundle\ContactBundle\Form;
 use Symfony\Component\Form\AbstractType,
     Symfony\Component\Form\FormBuilder;
 
+/**
+ * The contact form
+ *
+ * @author Yohan Giarelli <yohan@giarel.li>
+ */
 class ContactType extends AbstractType
 {
+    /**
+     * @param \Symfony\Component\Form\FormBuilder $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder
